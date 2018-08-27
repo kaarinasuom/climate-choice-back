b5b01976dc49cd4a5370a6e4661f6c40e7bf934a
anninahe <anninaheiskanen@gmail.com>
2018-08-27 10:01:40 +0300

    Aloitus

---
+++ src/main/java/fi/academy/climateswipe/ClimateswipeApplication.java
@@ -0,0 +1,13 @@
+package fi.academy.climateswipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimateswipeApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClimateswipeApplication.class, args);
+    }
+}
